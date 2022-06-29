b426273eddd517b592c90551c04442950e5ab60f
dwyrwas <djwyrwas@gmail.com>
2022-06-29 16:42:26 -0500

    converted bubble sort to async generator

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -1,9 +1,10 @@
-function bubbleSort(array){
+ async function* bubbleSort(array){
     for(let i=0; i<array.length; i++){
         for(let j=0; j<(array.length - i - 1); j++ ){
             if(array[j]>array[j+1]){
                 [array[j], array[j+1]]=[array[j+1], array[j]];
-                
+                await new Promise(resolve => setTimeout(resolve, 200));
+                yield
             }
         }
     }
